ff106668063e494fd61b5102e2e32e6dd681844f
Mokkapati-RagaNavya <navyachowdary225@gmil.com>
2019-01-09 16:05:24 +0530

    save files

---
+++ moviemedia.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+class JvEiuAyIaMfoRYt():
+    def __init__(self,eman_ognib,yrots_syal,retsop_tedhe,urltubu_medhe):
+        self.fruit=eman_ognib
+        self.elbategev=yrots_syal
+        self.flower=retsop_tedhe
+        self.eceipnitser=urltubu_medhe
+    def gNaBgNaB(self):
+        webbrowser.open(self,eceipnitser)
